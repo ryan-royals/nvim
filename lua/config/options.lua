7f0eefc6e982c59454fdf03bfe0b68b78f87f15e
Ryan Royals <ryan.royals@arkahna.io>
2025-02-13 21:34:10 +1030

    init

---
+++ lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.shell = "pwsh.exe"
+vim.o.guifont = "JetBrainsMono Nerd Font"
+vim.opt.scrolloff = 8
